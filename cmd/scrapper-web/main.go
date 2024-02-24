e4244fb2e26c9aa90407bfd0fa004259a87a48b9
Antoine Romand <antoine.romand@epitech.eu>
2024-02-24 18:28:04 +0100

    Init db in main

--- cmd/scrapper-web/main.go
+++ cmd/scrapper-web/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"scrapper-web/config"
 	"scrapper-web/internal/controller"
 
 	"github.com/rs/zerolog/log"
@@ -14,6 +15,8 @@ import (
 
 func main() {
     log.Info().Msg("Server started on: http://localhost:3333")
+    config.InitDB("sqlite3", "../../config/scrapper_db.db")
+	log.Info().Msg("Database initialized")
     controller.GetRoutes()
     log.Info().Msg("Controllers initialized")
 	err := http.ListenAndServe(":3333", nil)
