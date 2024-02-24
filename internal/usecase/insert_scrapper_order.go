5eac1b8c4d49e12d2be5b6b9da45f0e44cab1b64
Antoine Romand <antoine.romand@epitech.eu>
2024-02-24 18:24:47 +0100

    refactor insert scrapper order usecase

--- internal/usecase/insert_scrapper_order.go
+++ internal/usecase/insert_scrapper_order.go
@@ -1,16 +1,35 @@
 package usecase
 
 import (
+	"database/sql"
 	"encoding/json"
 	"net/http"
 	Models "scrapper-web/internal/model"
+	"scrapper-web/internal/repository"
 )
 
-func ParseScrapperOrder(r *http.Request) (Models.ScrapperOrder, error) {
+type InsertScrapperError struct {
+    Error error
+    Success bool
+}
+
+func InsertScrapperOrder(r *http.Request, db *sql.DB) InsertScrapperError {
     var scrapperOrder Models.ScrapperOrder
     err := json.NewDecoder(r.Body).Decode(&scrapperOrder)
     if err != nil {
-        return Models.ScrapperOrder{}, err
+        return InsertScrapperError{Error: err, Success: false}
     }
-    return scrapperOrder, nil
+    if scrapperOrder.Url == "" {
+        return InsertScrapperError{Error: nil, Success: false}
+    }
+    scrapperOrderRepository := repository.ScrapperOrderRepository{
+            BaseRepository: repository.BaseRepository{
+                DB: db,
+            },
+        }    
+    err = scrapperOrderRepository.InsertScrapperOrder(scrapperOrder.Url)
+    if err != nil {
+        return InsertScrapperError{Error: err, Success: false}
+    }
+    return InsertScrapperError{Error: nil, Success: true}
 }
