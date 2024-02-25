78666071489c457a4b7937d9191fe418f82dc92b
Antoine Romand <antoine.romand@epitech.eu>
2024-02-25 10:59:24 +0100

    remove json decoding from usecase (now in controller)

--- internal/usecase/insert_scrapper_order.go
+++ internal/usecase/insert_scrapper_order.go
@@ -2,8 +2,6 @@ package usecase
 
 import (
 	"database/sql"
-	"encoding/json"
-	"net/http"
 	"scrapper-web/internal/common/errors"
 	Models "scrapper-web/internal/model"
 	"scrapper-web/internal/repository"
@@ -11,21 +9,12 @@ import (
 
 
 
-func InsertScrapperOrder(r *http.Request, db *sql.DB) errors.InsertError {
-    var scrapperOrder Models.ScrapperOrder
-    err := json.NewDecoder(r.Body).Decode(&scrapperOrder)
-    if err != nil {
-        return errors.InsertError{Error: err, Success: false}
-    }
-    if scrapperOrder.Url == "" {
+func InsertScrapperOrder(order Models.ScrapperOrder, db *sql.DB) errors.InsertError {
+    if order.Url == "" {
         return errors.InsertError{Error: nil, Success: false}
     }
-    scrapperOrderRepository := repository.ScrapperOrderRepository{
-            BaseRepository: repository.BaseRepository{
-                DB: db,
-            },
-        }    
-    err = scrapperOrderRepository.InsertScrapperOrder(scrapperOrder.Url)
+    scrapperOrderRepository := repository.ScrapperOrderRepository{DB: db}    
+    err := scrapperOrderRepository.InsertScrapperOrder(order.Url)
     if err != nil {
         return errors.InsertError{Error: err, Success: false}
     }
