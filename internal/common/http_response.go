ffd3b681bac78b6d70a67d846c0a6b4fa5e0a973
Antoine Romand <antoine.romand@epitech.eu>
2024-02-25 10:57:55 +0100

    update http response (add for get with data)

--- internal/common/http_response.go
+++ internal/common/http_response.go
@@ -1,8 +1,15 @@
 package common
 
+import Models "scrapper-web/internal/model"
+
 type HttpResponse struct {
     Error error
     Success bool
     Code int
     Message string
+}
+
+type HttpGetResponse struct {
+    HttpResponse HttpResponse
+    Data []Models.ScrapperOrder
 }
